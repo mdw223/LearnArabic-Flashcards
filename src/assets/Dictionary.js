9acfc29f7b14797a6072a0a6fb726794d1d8e392
Malik <malikadidw@gmail.com>
2025-08-14 09:49:24 -0400

    updates on paths for images

--- src/assets/Dictionary.js
+++ src/assets/Dictionary.js
@@ -1,62 +1,70 @@
-// Dictionary.js
-
-// Dictionary.js
+import kitaabImg from './Kitaab.png';
+import salaamImg from './Salaam.png';
+import allahImg from './Allah.png';
+import ibrahimImg from './Ibrahim.png';
+import tawheedImg from './Tawheed.png';
+import salahImg from './Salah.png';
+import sawmImg from './Sawm.png';
+import akhirahImg from './Akhirah.png';
+import quranImg from './Quran.png';
+import zakatImg from './Zakat.png';
+import shahadaImg from './Shahada.png';
 
 const dictionary = [
     {
         eng: "What is the Arabic word for 'book'?",
         ar: "Kitaab (كتاب)",
-        pic: "src/assets/Kitaab.png"
+        pic: kitaabImg
     },
     {
         eng: "What is the Arabic word for 'peace'?",
         ar: "Salaam (سلام)",
-        pic: "src/assets/Salaam.png" 
+        pic: salaamImg 
     },
     {
         eng: "What is the Arabic word for 'God'?",
         ar: "Allah (الله)",
-        pic: "src/assets/Allah.png" 
+        pic: allahImg 
     },
     {
         eng: "What is the Arabic word for 'Abraham'?",
         ar: "Ibrahim (إبراهيم)",
-        pic: "src/assets/Ibrahim.png" 
+        pic: ibrahimImg 
     },
     {
         eng: "What is the Arabic word for the concept of 'oneness of God'?",
         ar: "Tawheed (توحيد)",
-        pic: "src/assets/Tawheed.png" 
+        pic: tawheedImg 
     },
     {
         eng: "What is the Arabic word for 'prayer'?",
         ar: "Salah (صلاة)",
-        pic: "src/assets/Salah.png" 
+        pic: salahImg 
     },
     {
         eng: "What is the Arabic word for 'fasting'?",
         ar: "Sawm (صوم)",
-        pic: "src/assets/Sawm.png" 
+        pic: sawmImg 
     },
     {
         eng: "What is the Arabic word for 'hereafter'?",
         ar: "Akhirah (آخرة)",
-        pic: "src/assets/Akhirah.png" 
+        pic: akhirahImg 
     },
     {
         eng: "What is the holy book revealed to prophet Muhammad (peace be upon him)?",
         ar: "Quran (قرآن)",
-        pic: "src/assets/Quran.png" 
+        pic: quranImg 
     },
     {
         eng: "What is the Arabic word for 'charity to the needy'?",
         ar: "Zakat (زكاة)",
-        pic: "src/assets/Zakat.png" 
+        pic: zakatImg 
     },
     {
         eng: "What is the Arabic word for 'declaration of faith' to become Muslim?",
         ar: "Shahada (شهادة)",
-        pic: "src/assets/Shahada.png" 
+        pic: shahadaImg 
     }
 ];
 
